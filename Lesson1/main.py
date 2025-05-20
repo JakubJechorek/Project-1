ee3fd49b8db9887bcc1a15a49f03d6d466974f94
Jakub Jechorek <jakubjechorek15@gmail.com>
2025-05-20 10:26:48 +0100

    main

---
+++ Lesson1/main.py
@@ -0,0 +1,12 @@
+#Prompt to gather required info:
+
+recipient_name = input("Enter the recipient's name: ")
+year_of_birth = int(input("Enter the recipient's birth year: "))
+personal_message = input("Enter a short personalized message: ")
+sender_name = input("Enter Senders Name: ")
+
+#calculations to generate response:
+current_year = 2025
+age = current_year - year_of_birth
+
+print(f"{recipient_name}, let's celebrate your {age} years of awesomeness!\nWishing you a day filled with joy and laughter as you turn {age}!\n{personal_message}\n{sender_name}")
